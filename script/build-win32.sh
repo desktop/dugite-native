7460625728cf0e019d7eed6eb14d7f1708b0743d
Brendan Forster <brendan@github.com>
2017-04-09 20:43:49 +1000

    adapt scripts for AppVeyor environment

--- script/build-win32.sh
+++ script/build-win32.sh
@@ -9,7 +9,12 @@ mkdir -p $DESTINATION
 # download Git for Windows, verify its the right contents, and unpack it
 GIT_FOR_WINDOWS_FILE=git-for-windows.zip
 curl -sL -o $GIT_FOR_WINDOWS_FILE $GIT_FOR_WINDOWS_URL
-COMPUTED_SHA256=$(shasum -a 256 $GIT_FOR_WINDOWS_FILE | awk '{print $1;}')
+if [ "$APPVEYOR" == "True" ]; then
+  COMPUTED_SHA256=$(sha256sum $GIT_FOR_WINDOWS_FILE | awk '{print $1;}')
+else
+  COMPUTED_SHA256=$(shasum -a 256 $GIT_FOR_WINDOWS_FILE | awk '{print $1;}')
+fi
+
 if [ "$COMPUTED_SHA256" = "$GIT_FOR_WINDOWS_CHECKSUM" ]; then
   echo "Git for Windows: checksums match"
   unzip -qq $GIT_FOR_WINDOWS_FILE -d $DESTINATION
@@ -22,7 +27,11 @@ fi
 # download Git LFS, verify its the right contents, and unpack it
 GIT_LFS_FILE=git-lfs.zip
 curl -sL -o $GIT_LFS_FILE $GIT_LFS_URL
-COMPUTED_SHA256=$(shasum -a 256 $GIT_LFS_FILE | awk '{print $1;}')
+if [ "$APPVEYOR" == "True" ]; then
+  COMPUTED_SHA256=$(sha256sum $GIT_LFS_FILE | awk '{print $1;}')
+else
+  COMPUTED_SHA256=$(shasum -a 256 $GIT_LFS_FILE | awk '{print $1;}')
+fi
 if [ "$COMPUTED_SHA256" = "$GIT_LFS_CHECKSUM" ]; then
   echo "Git LFS: checksums match"
   SUBFOLDER="$DESTINATION/mingw64/libexec/git-core/"
