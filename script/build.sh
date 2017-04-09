ce3c471fa28291d5a10bdd5f9ead281b1679212f
Brendan Forster <brendan@github.com>
2017-04-09 20:42:55 +1000

    rename PLATFORM to TARGET_PLATFORM as AppVeyor uses Platform which leads to some case-sensitive madness when working on Windows

--- script/build.sh
+++ script/build.sh
@@ -9,13 +9,13 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 SOURCE="./git"
 DESTINATION="/tmp/build/git"
 
-if [ "$PLATFORM" == "ubuntu" ]; then
+if [ "$TARGET_PLATFORM" == "ubuntu" ]; then
   sh "$DIR/build-ubuntu.sh" $SOURCE $DESTINATION
-elif [ "$PLATFORM" == "macOS" ]; then
+elif [ "$TARGET_PLATFORM" == "macOS" ]; then
   sh "$DIR/build-macos.sh" $SOURCE $DESTINATION
-elif [ "$PLATFORM" == "win32" ]; then
+elif [ "$TARGET_PLATFORM" == "win32" ]; then
   sh "$DIR/build-win32.sh" $DESTINATION
 else
-  echo "Unable to build Git for platform $PLATFORM"
+  echo "Unable to build Git for platform $TARGET_PLATFORM"
   exit 1
 fi
