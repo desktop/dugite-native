12ab533b4984d53ac6c04b51658c3d99fcafb79e
Brendan Forster <brendan@github.com>
2017-04-10 11:44:20 +1000

    testing all the things

--- script/build-win32.sh
+++ script/build-win32.sh
@@ -49,4 +49,30 @@ WINSSL_CURL_LIBRARY="$DESTINATION/mingw64/bin/curl-winssl/libcurl-4.dll"
 mv $WINSSL_CURL_LIBRARY $ORIGINAL_CURL_LIBRARY
 rm -rf "$DESTINATION/mingw64/bin/curl-winssl/"
 
-# TODO: validate change
+if [ "$APPVEYOR" == "True" ]; then
+  # find the version of libcurl that was bundled
+  PACKAGE_ENTRY=$(grep -o 'curl [0-9].[0-9]\{2\}.[0-9]-[0-9]' /tmp/build/git/etc/package-versions.txt)
+  PACKAGE_VERSION=${PACKAGE_ENTRY/curl /}
+
+  echo "Using curl version version $PACKAGE_VERSION"
+  CURL_FILE="curl.tar.xz"
+  CURL_BINARY_DOWNLOAD="https://dl.bintray.com/git-for-windows/pacman/x86_64/mingw-w64-x86_64-curl-$PACKAGE_VERSION-any.pkg.tar.xz"
+  curl -sL -o $CURL_FILE $CURL_BINARY_DOWNLOAD
+
+  # extract just the executable to test alongside our libcurl-4.dll change
+  7z e $CURL_FILE -aoa -ooutput > nul
+  7z e output/curl.tar -aoa -ooutput *.exe -r > nul
+  cp ./output/curl.exe "$DESTINATION/mingw64/bin/"
+  rm -rf ./output
+
+  $DESTINATION/mingw64/bin/curl.exe --version | grep 'WinSSL'
+  EXIT_CODE=$?
+  rm $DESTINATION/mingw64/bin/curl.exe
+
+  if [ "$EXIT_CODE" == "1" ]; then
+    echo "cURL not able to resolve WinSSL dependency. Failing the build..."
+    exit 1
+  else
+    echo "Verified cURL dependency is using WinSSL"
+  fi
+fi
