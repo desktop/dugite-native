5410abb3f1f11c56bade030174ca30020a8ba6cf
Brendan Forster <brendan@github.com>
2017-04-30 12:01:24 +1000

    make this executable?

---
+++ script/setup.sh
@@ -0,0 +1,17 @@
+# a more graceful way to compute checksums
+
+computeChecksum() {
+   if [ -z "$1" ] ; then
+     # no parameter provided, fail hard
+     exit 1
+   fi
+
+  path_to_sha256sum=$(which sha256sum)
+  if [ -x "$path_to_sha256sum" ] ; then
+    echo $(sha256sum $1 | awk '{print $1;}')
+  else
+    echo $(shasum -a 256 $1 | awk '{print $1;}')
+  fi
+}
+
+export -f computeChecksum
