7bb6446dbc9829e2895b07915eb63b272c1d437d
Brendan Forster <brendan@github.com>
2017-06-04 08:54:21 -0300

    refresh test scripts

--- test/ubuntu.sh
+++ test/ubuntu.sh
@@ -2,7 +2,22 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOT="$DIR/.."
+SOURCE="$ROOT/git"
+DESTINATION="$ROOT/build/git"
 
 GIT_LFS_URL=https://github.com/git-lfs/git-lfs/releases/download/v2.1.0/git-lfs-linux-amd64-2.1.0.tar.gz \
 GIT_LFS_CHECKSUM=0260d1908b097dcd703ef6cf83d9c32c1a418325d29b063bf03a165e3dd8e364 \
-. "$ROOT/script/build-ubuntu.sh" "$ROOT/git" "$ROOT/build/git/"
+. "$ROOT/script/build-ubuntu.sh" $SOURCE $DESTINATION
+
+FILE="dugite-native-$VERSION-ubuntu-test.tar.gz"
+
+tar -czf $FILE -C $DESTINATION .
+
+echo "Archive contents:"
+cd $DESTINATION
+du -ah $DESTINATION
+cd - > /dev/null
+
+echo ""
+SIZE=$(du -h $FILE | cut -f1)
+echo "Package size: ${SIZE}"
