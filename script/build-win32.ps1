b40725133b82033bdb18959d3c399cc83da5ef81
Markus Olsson <j.markus.olsson@gmail.com>
2017-06-19 13:37:27 +0200

    Add initial proof of concept build script for Windows

---
+++ script/build-win32.ps1
@@ -0,0 +1,37 @@
+# https://stackoverflow.com/a/34559554
+function New-TemporaryDirectory {
+    $parent = [System.IO.Path]::GetTempPath()
+    [string] $name = [System.Guid]::NewGuid()
+    [string] $path = (Join-Path $parent $name)
+    New-Item -ItemType Directory -Path $path
+}
+
+$git_sdk_version="1.0.3"
+$git_sdk_tag="git-sdk-$git_sdk_version"
+$git_version="v2.13.1.windows.2"
+$mingit_version="2.13.1.2"
+
+Write-Output $tempDir
+Set-Location -Path (New-TemporaryDirectory).FullName
+
+Invoke-WebRequest -Uri "http://freakcode.s3.amazonaws.com/7za-x64.exe" -OutFile 7za.exe
+Invoke-WebRequest -Uri "https://github.com/git-for-windows/build-extra/releases/download/$git_sdk_tag/git-sdk-installer-1.0.3-64.7z.exe" -OutFile git-sdk-installer.7z.exe
+
+& .\7za.exe x -oC:\git-sdk-64 git-sdk-installer.7z.exe
+
+Set-Location -Path c:\git-sdk-64
+
+# Setting this will prevent the setup-git-sdk script from actually cloning and building
+# the source. We'll do that ourselves as we need to do some patching.
+$env:JENKINS_URL="http://127.0.0.1"
+
+& .\setup-git-sdk.bat
+
+# set MSYSTEM so that MSYS2 starts up in the correct mode
+$env:MSYSTEM = "MINGW64"
+
+$bash = ".\usr\bin\bash.exe"
+
+& $bash --login -c "mkdir -p /usr/src && cd /usr/src && for project in MINGW-packages MSYS2-packages build-extra git; do test ! -d `$project && (git clone -b master -c core.autocrlf=false https://github.com/git-for-windows/`$project); done"
+& $bash --login -c "cd /usr/src/git && make install"
+& $bash --login -c "/usr/src/build-extra/mingit/release.sh --output=/c/Users/Administrator/Documents/ $mingit_version"
