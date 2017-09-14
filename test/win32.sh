a0d4828a90ea6212aacd868770857f2937a24369
joshaber <joshaber@gmail.com>
2017-09-14 15:04:59 -0400

    Update to LFS 2.3.0

--- test/win32.sh
+++ test/win32.sh
@@ -7,8 +7,8 @@ DESTINATION="$ROOT/build/git"
 
 GIT_FOR_WINDOWS_URL=https://github.com/git-for-windows/git/releases/download/v2.14.1.windows.1/MinGit-2.14.1-64-bit.zip \
 GIT_FOR_WINDOWS_CHECKSUM=65c12e4959b8874187b68ec37e532fe7fc526e10f6f0f29e699fa1d2449e7d92 \
-GIT_LFS_URL=https://github.com/git-lfs/git-lfs/releases/download/v2.2.1/git-lfs-windows-amd64-2.2.1.zip \
-GIT_LFS_CHECKSUM=35e120c03061c7a3de8348b970da2278a2e0a865d4c67179801266a2d7674d2d \
+GIT_LFS_URL=https://github.com/git-lfs/git-lfs/releases/download/v2.3.0/git-lfs-windows-amd64-2.3.0.zip \
+GIT_LFS_CHECKSUM=0be7e8755e1c2d9a598f369ce1db63fd7f2a8985d1c078cb815a3e50961066c2 \
 . "$ROOT/script/build-win32.sh" $SOURCE $DESTINATION
 
 FILE="dugite-native-$VERSION-win32-test.tar.gz"
