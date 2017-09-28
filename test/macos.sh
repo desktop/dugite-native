212ba155caf1c3da746c06d530d7079ea072e066
Brendan Forster <brendan@github.com>
2017-09-28 15:12:12 +1000

    update test harnesses

--- test/macos.sh
+++ test/macos.sh
@@ -5,8 +5,8 @@ ROOT="$DIR/.."
 SOURCE="$ROOT/git"
 DESTINATION="$ROOT/build/git"
 
-GIT_LFS_URL=https://github.com/git-lfs/git-lfs/releases/download/v2.3.0/git-lfs-darwin-amd64-2.3.0.tar.gz \
-GIT_LFS_CHECKSUM=37d588897194fe959d8d39bae1f057d486be53e0f2f7252abeacfd8aa31da9ee \
+GIT_LFS_URL=https://github.com/git-lfs/git-lfs/releases/download/v2.3.1/git-lfs-darwin-amd64-2.3.1.tar.gz \
+GIT_LFS_CHECKSUM=5b4f81f4afc1447776dcfeaf5ff63fb0b5ea522ccac587aa97942203ac977e0f \
 . "$ROOT/script/build-macos.sh" $SOURCE $DESTINATION
 
 FILE="dugite-native-$VERSION-macos-test.tar.gz"
