d22d1d69b96c293c27128706f5455d276eb92af6
Brendan Forster <github@brendanforster.com>
2017-12-17 21:51:33 +1030

    update test harness script

--- script/update-test-harness.js
+++ script/update-test-harness.js
@@ -17,18 +17,38 @@ DESTINATION="$ROOT/build/git"
 ${environmentVariables}
 . "$ROOT/script/${script}" $SOURCE $DESTINATION
 
-FILE="dugite-native-$VERSION-${os}-test.tar.gz"
-
-tar -czf $FILE -C $DESTINATION .
-
 echo "Archive contents:"
 cd $DESTINATION
 du -ah $DESTINATION
 cd - > /dev/null
 
+GZIP_FILE="dugite-native-$VERSION-${os}-test.tar.gz"
+LZMA_FILE="dugite-native-$VERSION-${os}-test.lzma"
+
 echo ""
-SIZE=$(du -h $FILE | cut -f1)
-echo "Package size: \${SIZE}"`
+echo "Creating archives..."
+if [ "$(uname -s)" == "Darwin" ]; then
+  tar -czf $GZIP_FILE -C $DESTINATION .
+  tar --lzma -cf $LZMA_FILE -C $DESTINATION .
+else
+  tar -caf $GZIP_FILE -C $DESTINATION .
+  tar -caf $LZMA_FILE -C $DESTINATION .
+fi
+
+if [ "$APPVEYOR" == "True" ]; then
+  GZIP_CHECKSUM=$(sha256sum $GZIP_FILE | awk '{print $1;}')
+  LZMA_CHECKSUM=$(sha256sum $LZMA_FILE | awk '{print $1;}')
+else
+  GZIP_CHECKSUM=$(shasum -a 256 $GZIP_FILE | awk '{print $1;}')
+  LZMA_CHECKSUM=$(shasum -a 256 $LZMA_FILE | awk '{print $1;}')
+fi
+
+GZIP_SIZE=$(du -h $GZIP_FILE | cut -f1)
+LZMA_SIZE=$(du -h $LZMA_FILE | cut -f1)
+
+echo "Packages created:"
+echo "\${GZIP_FILE} - \${GZIP_SIZE} - checksum: \${GZIP_CHECKSUM}"
+echo "\${LZMA_FILE} - \${LZMA_SIZE} - checksum: \${LZMA_CHECKSUM}"`
 
   const destination = path.resolve(__dirname, '..', `test/${os}.sh`)
   fs.writeFileSync(destination, fileContents, { encoding: 'utf-8', mode: '777' })
