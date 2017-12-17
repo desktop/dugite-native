ce525dd6edb966237b1ce3f897bab8cde236460a
Brendan Forster <github@brendanforster.com>
2017-12-17 21:51:45 +1030

    regenerate test harness

--- test/macos.sh
+++ test/macos.sh
@@ -9,15 +9,35 @@ GIT_LFS_URL=https://github.com/git-lfs/git-lfs/releases/download/v2.3.4/git-lfs-
 GIT_LFS_CHECKSUM=b16d4b7469b1fa34e0e27bedb1b77cc425b8d7903264854e5f18b0bc73576edb \
 . "$ROOT/script/build-macos.sh" $SOURCE $DESTINATION
 
-FILE="dugite-native-$VERSION-macos-test.tar.gz"
-
-tar -czf $FILE -C $DESTINATION .
-
 echo "Archive contents:"
 cd $DESTINATION
 du -ah $DESTINATION
 cd - > /dev/null
 
+GZIP_FILE="dugite-native-$VERSION-macos-test.tar.gz"
+LZMA_FILE="dugite-native-$VERSION-macos-test.lzma"
+
 echo ""
-SIZE=$(du -h $FILE | cut -f1)
-echo "Package size: ${SIZE}"
+echo "Creating archives..."
+if [ "$(uname -s)" == "Darwin" ]; then
+  tar -czf $GZIP_FILE -C $DESTINATION .
+  tar --lzma -cf $LZMA_FILE -C $DESTINATION .
+else
+  tar -caf $GZIP_FILE -C $DESTINATION .
+  tar -caf $LZMA_FILE -C $DESTINATION .
+fi
+
+if [ "$APPVEYOR" == "True" ]; then
+  GZIP_CHECKSUM=$(sha256sum $GZIP_FILE | awk '{print $1;}')
+  LZMA_CHECKSUM=$(sha256sum $LZMA_FILE | awk '{print $1;}')
+else
+  GZIP_CHECKSUM=$(shasum -a 256 $GZIP_FILE | awk '{print $1;}')
+  LZMA_CHECKSUM=$(shasum -a 256 $LZMA_FILE | awk '{print $1;}')
+fi
+
+GZIP_SIZE=$(du -h $GZIP_FILE | cut -f1)
+LZMA_SIZE=$(du -h $LZMA_FILE | cut -f1)
+
+echo "Packages created:"
+echo "${GZIP_FILE} - ${GZIP_SIZE} - checksum: ${GZIP_CHECKSUM}"
+echo "${LZMA_FILE} - ${LZMA_SIZE} - checksum: ${LZMA_CHECKSUM}"
--- test/ubuntu.sh
+++ test/ubuntu.sh
@@ -9,15 +9,35 @@ GIT_LFS_URL=https://github.com/git-lfs/git-lfs/releases/download/v2.3.4/git-lfs-
 GIT_LFS_CHECKSUM=6755e109a85ffd9a03aacc629ea4ab1cbb8e7d83e41bd1880bf44b41927f4cfe \
 . "$ROOT/script/build-ubuntu.sh" $SOURCE $DESTINATION
 
-FILE="dugite-native-$VERSION-ubuntu-test.tar.gz"
-
-tar -czf $FILE -C $DESTINATION .
-
 echo "Archive contents:"
 cd $DESTINATION
 du -ah $DESTINATION
 cd - > /dev/null
 
+GZIP_FILE="dugite-native-$VERSION-ubuntu-test.tar.gz"
+LZMA_FILE="dugite-native-$VERSION-ubuntu-test.lzma"
+
 echo ""
-SIZE=$(du -h $FILE | cut -f1)
-echo "Package size: ${SIZE}"
+echo "Creating archives..."
+if [ "$(uname -s)" == "Darwin" ]; then
+  tar -czf $GZIP_FILE -C $DESTINATION .
+  tar --lzma -cf $LZMA_FILE -C $DESTINATION .
+else
+  tar -caf $GZIP_FILE -C $DESTINATION .
+  tar -caf $LZMA_FILE -C $DESTINATION .
+fi
+
+if [ "$APPVEYOR" == "True" ]; then
+  GZIP_CHECKSUM=$(sha256sum $GZIP_FILE | awk '{print $1;}')
+  LZMA_CHECKSUM=$(sha256sum $LZMA_FILE | awk '{print $1;}')
+else
+  GZIP_CHECKSUM=$(shasum -a 256 $GZIP_FILE | awk '{print $1;}')
+  LZMA_CHECKSUM=$(shasum -a 256 $LZMA_FILE | awk '{print $1;}')
+fi
+
+GZIP_SIZE=$(du -h $GZIP_FILE | cut -f1)
+LZMA_SIZE=$(du -h $LZMA_FILE | cut -f1)
+
+echo "Packages created:"
+echo "${GZIP_FILE} - ${GZIP_SIZE} - checksum: ${GZIP_CHECKSUM}"
+echo "${LZMA_FILE} - ${LZMA_SIZE} - checksum: ${LZMA_CHECKSUM}"
