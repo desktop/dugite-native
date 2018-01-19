7d42413eec5d357fc657e1ec6a6d86e217bd5c2b
Brendan Forster <github@brendanforster.com>
2018-01-18 16:25:47 -0800

    update test harness

--- test/win32.sh
+++ test/win32.sh
@@ -5,8 +5,8 @@ ROOT="$DIR/.."
 SOURCE="$ROOT/git"
 DESTINATION="$ROOT/build/git"
 
-GIT_FOR_WINDOWS_URL=https://github.com/git-for-windows/git/releases/download/v2.16.0-rc1.windows.1/MinGit-2.16.0.rc1.windows.1-64-bit.zip \
-GIT_FOR_WINDOWS_CHECKSUM=9feafbb5ea0975ab2bcdea7365fffec53dba457a81572bbf091dc28aa538060f \
+GIT_FOR_WINDOWS_URL=https://github.com/git-for-windows/git/releases/download/v2.16.0.windows.2/MinGit-2.16.0.2-64-bit.zip \
+GIT_FOR_WINDOWS_CHECKSUM=fb028d2a18c7ec18f8febecafc95e9dad0dd583ab8fe376c95a06eff62058bbd \
 GIT_LFS_URL=https://github.com/git-lfs/git-lfs/releases/download/v2.3.4/git-lfs-windows-amd64-2.3.4.zip \
 GIT_LFS_CHECKSUM=18c47fd2806659e81a40fbd6f6b0598ea1802635ce04fb2317d75973450a3fe5 \
 . "$ROOT/script/build-win32.sh" $SOURCE $DESTINATION
