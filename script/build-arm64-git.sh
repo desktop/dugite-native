8605fd1de83ba2f563838bea11cba063735d5089
Brendan Forster <github@brendanforster.com>
2018-07-23 10:19:43 -0300

    ensure libssl is used when compiling on ARM64

--- script/build-arm64-git.sh
+++ script/build-arm64-git.sh
@@ -1,6 +1,6 @@
 echo " -- Building git at $SOURCE to $DESTINATION"
 apt-get update
-apt-get install -y build-essential libexpat-dev libcurl4-openssl-dev zlib1g-dev
+apt-get install -y build-essential libexpat-dev libcurl4-openssl-dev zlib1g-dev libssl-dev
 
 cd $SOURCE
 make clean
