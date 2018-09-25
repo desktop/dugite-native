57f898117cf561205d67a2d6d3d5d8e0ad49a9fb
Brendan Forster <github@brendanforster.com>
2018-09-25 17:15:08 -0300

    actually generate the template

--- script/generate-release-notes.js
+++ script/generate-release-notes.js
@@ -1,5 +1,10 @@
 const octokit = require("@octokit/rest")();
 
+// five targeted OS/arch combinations
+// two files for each targeted OS/arch
+// two checksum files for the previous
+const SUCCESSFUL_RELEASE_FILE_COUNT = 5 * 2 * 2;
+
 process.on("unhandledRejection", (reason, p) => {
   console.log("Unhandled Rejection, reason:", reason);
 });
@@ -38,21 +43,51 @@ async function run() {
   console.log(`tag name: ${release.data.tag_name}`);
   console.log(`draft: ${release.data.draft}`);
 
+  const tag = release.data.tag_name;
+
+  if (release.data.draft === false) {
+    throw new Error(
+      `Latest published release ${tag} is not a draft. Aborting...`
+    );
+  }
+
   const assets = await octokit.repos.getAssets({
     owner: "desktop",
     repo: "dugite-native",
     release_id: release.data.id
   });
 
+  if (assets.data.length !== SUCCESSFUL_RELEASE_FILE_COUNT) {
+    throw new Error(
+      `Latest draft release ${tag} has ${
+        assets.data.length
+      } files, expected ${SUCCESSFUL_RELEASE_FILE_COUNT}. Aborting...`
+    );
+  }
+
+  const entries = [];
+
   for (const asset of assets.data) {
     const { name, browser_download_url } = asset;
     if (name.endsWith(".sha256")) {
       const fileName = name.slice(0, -3);
       console.log(`found SHA256 file ${name} for ${fileName}`);
+      const checksum = downloadFile(browser_download_url);
+      entries.push({ fileName, checksum });
     } else {
-      console.log(`skipping: ${name}`);
+      console.log(`skipping file: ${name}`);
     }
   }
+
+  const fileList = entries.map(e => `| ${e.fileName} | ${e.checksum} |`);
+  const fileListText = fileList.join("\n");
+  const draftReleaseNotes = `{details about what's changed since the last release}
+
+| File | SHA256 checksum |
+| --- | --- |
+${fileListText}`;
+
+  console.log(`draft release: ${draftReleaseNotes}`);
 }
 
 run();
