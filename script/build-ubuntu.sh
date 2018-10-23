e4cee338985b9ec2fa3f9171c1888898b7e857d4
Brendan Forster <github@brendanforster.com>
2018-10-23 11:34:20 -0300

    linux: test clone with generated binary

--- script/build-ubuntu.sh
+++ script/build-ubuntu.sh
@@ -143,3 +143,20 @@ do
   checkStaticLinking $file
 done
 cd - > /dev/null
+
+echo "-- Testing clone operation with generated binary"
+
+rm -rf $CURL_OUTPUT_DIR
+
+TEMP_CLONE_DIR=/tmp/clones
+mkdir -p $TEMP_CLONE_DIR
+
+cd "$DESTINATION/bin"
+./git --version
+GIT_CURL_VERBOSE=1 \
+  GIT_TEMPLATE_DIR="$DESTINATION/share/git-core/templates" \
+  GIT_SSL_CAINFO="$DESTINATION/ssl/cacert.pem" \
+  GIT_EXEC_PATH="$DESTINATION/libexec/git-core" \
+  PREFIX="$DESTINATION" \
+  ./git clone https://github.com/git/git.github.io "$TEMP_CLONE_DIR/git.github.io"
+cd - > /dev/null
