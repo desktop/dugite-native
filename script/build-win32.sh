82d20470e54fb15538c6fd4746278c275f917a88
Brendan Forster <github@brendanforster.com>
2018-11-04 11:05:42 -0400

    address suggestions in windows build script

--- script/build-win32.sh
+++ script/build-win32.sh
@@ -9,17 +9,17 @@ CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # shellcheck source=script/compute-checksum.sh
 source "$CURRENT_DIR/compute-checksum.sh"
 
-mkdir -p $DESTINATION
+mkdir -p "$DESTINATION"
 
 if [ "$WIN_ARCH" -eq "64" ]; then MINGW_DIR="mingw64"; else MINGW_DIR="mingw32"; fi
 
 echo "-- Downloading MinGit from $GIT_FOR_WINDOWS_URL"
 GIT_FOR_WINDOWS_FILE=git-for-windows.zip
-curl -sL -o $GIT_FOR_WINDOWS_FILE $GIT_FOR_WINDOWS_URL
+curl -sL -o $GIT_FOR_WINDOWS_FILE "$GIT_FOR_WINDOWS_URL"
 COMPUTED_SHA256=$(compute_checksum $GIT_FOR_WINDOWS_FILE)
 if [ "$COMPUTED_SHA256" = "$GIT_FOR_WINDOWS_CHECKSUM" ]; then
   echo "MinGit: checksums match"
-  unzip -qq $GIT_FOR_WINDOWS_FILE -d $DESTINATION
+  unzip -qq $GIT_FOR_WINDOWS_FILE -d "$DESTINATION"
 else
   echo "MinGit: expected checksum $GIT_FOR_WINDOWS_CHECKSUM but got $COMPUTED_SHA256"
   echo "aborting..."
@@ -34,12 +34,12 @@ if [[ "$GIT_LFS_VERSION" ]]; then
   if [ "$WIN_ARCH" -eq "64" ]; then GIT_LFS_ARCH="amd64"; else GIT_LFS_ARCH="386"; fi
   GIT_LFS_URL="https://github.com/git-lfs/git-lfs/releases/download/v${GIT_LFS_VERSION}/git-lfs-windows-${GIT_LFS_ARCH}-v${GIT_LFS_VERSION}.zip"
   echo "-- Downloading from $GIT_LFS_URL"
-  curl -sL -o $GIT_LFS_FILE $GIT_LFS_URL
+  curl -sL -o $GIT_LFS_FILE "$GIT_LFS_URL"
   COMPUTED_SHA256=$(compute_checksum $GIT_LFS_FILE)
   if [ "$COMPUTED_SHA256" = "$GIT_LFS_CHECKSUM" ]; then
     echo "Git LFS: checksums match"
     SUBFOLDER="$DESTINATION/$MINGW_DIR/libexec/git-core"
-    unzip -j $GIT_LFS_FILE -x '*.md' -d $SUBFOLDER
+    unzip -j $GIT_LFS_FILE -x '*.md' -d "$SUBFOLDER"
 
     # this is a workaround because Git LFS changed the names of the files in the archive
     OLD_FILE_NAME="git-lfs-windows-$GIT_LFS_ARCH.exe"
@@ -63,16 +63,16 @@ fi
 SYSTEM_CONFIG="$DESTINATION/$MINGW_DIR/etc/gitconfig"
 
 echo "-- Setting some system configuration values"
-git config --file $SYSTEM_CONFIG core.symlinks "false"
-git config --file $SYSTEM_CONFIG core.autocrlf "true"
-git config --file $SYSTEM_CONFIG core.fscache "true"
-git config --file $SYSTEM_CONFIG http.sslBackend "schannel"
+git config --file "$SYSTEM_CONFIG" core.symlinks "false"
+git config --file "$SYSTEM_CONFIG" core.autocrlf "true"
+git config --file "$SYSTEM_CONFIG" core.fscache "true"
+git config --file "$SYSTEM_CONFIG" http.sslBackend "schannel"
 
 # See https://github.com/desktop/desktop/issues/4817#issuecomment-393241303
 # Even though it's not set openssl will auto-discover the one we ship because
 # it sits in the right location already. So users manually switching
 # http.sslBackend to openssl will still pick it up.
-git config --file $SYSTEM_CONFIG --unset http.sslCAInfo
+git config --file "$SYSTEM_CONFIG" --unset http.sslCAInfo
 
 # Git for Windows 2.18.1 will support controlling how curl uses any certificate
 # bundle - rather than just loading the bundle if http.useSSLCAInfo is set
@@ -80,7 +80,7 @@ git config --file $SYSTEM_CONFIG --unset http.sslCAInfo
 # user has overriden this in their global configuration.
 #
 # details: https://github.com/dscho/git/blob/6152657e1a97c478df97d633c47469043b397519/Documentation/config.txt#L2135
-git config --file $SYSTEM_CONFIG http.schannelUseSSLCAInfo "false"
+git config --file "$SYSTEM_CONFIG" http.schannelUseSSLCAInfo "false"
 
 # removing global gitattributes file
 rm "$DESTINATION/$MINGW_DIR/etc/gitattributes"
