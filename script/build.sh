72cb5ae3a64be616acdfba31bbb821723eadba6e
Brendan Forster <github@brendanforster.com>
2018-11-04 11:51:00 -0400

    build script can now omit parameters

--- script/build.sh
+++ script/build.sh
@@ -4,20 +4,23 @@
 # Platforms have different ways to build Git and prepare the environment
 # for packaging, so defer to the `build-*` files for more details
 #
-BASEDIR=$(pwd)
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# shellcheck disable=SC2034
 SOURCE="${BASEDIR}/git"
+# shellcheck disable=SC2034
 DESTINATION="/tmp/build/git"
+# shellcheck disable=SC2034
 CURL_INSTALL_DIR="/tmp/build/curl"
 
 if [ "$TARGET_PLATFORM" == "ubuntu" ]; then
-  bash "$DIR/build-ubuntu.sh" "$SOURCE" $DESTINATION $CURL_INSTALL_DIR
+  bash "$CURRENT_DIR/build-ubuntu.sh"
 elif [ "$TARGET_PLATFORM" == "macOS" ]; then
-  bash "$DIR/build-macos.sh" "$SOURCE" $DESTINATION
+  bash "$CURRENT_DIR/build-macos.sh"
 elif [ "$TARGET_PLATFORM" == "win32" ]; then
-  bash "$DIR/build-win32.sh" $DESTINATION
+  bash "$CURRENT_DIR/build-win32.sh"
 elif [ "$TARGET_PLATFORM" == "arm64" ]; then
-  bash "$DIR/build-arm64.sh" "$SOURCE" "$DESTINATION" "$CURL_INSTALL_DIR" "$BASEDIR"
+  BASEDIR=$(pwd)
+  bash "$CURRENT_DIR/build-arm64.sh"
 else
   echo "Unable to build Git for platform $TARGET_PLATFORM"
   exit 1
