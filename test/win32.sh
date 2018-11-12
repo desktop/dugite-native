cd34f17e9e53d8f2d579c1c9286de7f370e534fd
Brendan Forster <github@brendanforster.com>
2018-11-12 17:03:54 -0400

    regenerate test scripts

--- test/win32.sh
+++ test/win32.sh
@@ -1,46 +1,91 @@
 #!/bin/bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-ROOT="$DIR/.."
+GIT_LFS_VERSION=2.6.0
+TARGET_PLATFORM=win32
+WIN_ARCH=32
+GIT_FOR_WINDOWS_URL=https://github.com/git-for-windows/git/releases/download/v2.19.1.windows.1/MinGit-2.19.1-32-bit.zip
+GIT_FOR_WINDOWS_CHECKSUM=9bde728fe03f66a022b3e41408902ccfceb56a34067db1f35d6509375b9be922
+GIT_LFS_CHECKSUM=7fa3475c60221837860138b4fd0fd0ad1213a5e49c596fdb0aac8932ca7a20a5
+
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ROOT="$CURRENT_DIR/.."
 SOURCE="$ROOT/git"
 DESTINATION="$ROOT/build/git"
 
-GIT_LFS_VERSION=2.5.2 \
-TARGET_PLATFORM=win32 \
-WIN_ARCH=32 \
-GIT_FOR_WINDOWS_URL=https://github.com/git-for-windows/git/releases/download/v2.19.1.windows.1/MinGit-2.19.1-32-bit.zip \
-GIT_FOR_WINDOWS_CHECKSUM=9bde728fe03f66a022b3e41408902ccfceb56a34067db1f35d6509375b9be922 \
-GIT_LFS_CHECKSUM=6cf7d4c169a17dd5b326f903708829e7471368b7e1235ab150ce77555f47b213 \
 . "$ROOT/script/build-win32.sh" $SOURCE $DESTINATION
+
+source "$ROOT/script/compute-checksum.sh"
+
+VERSION=$(
+  cd $SOURCE || exit 1
+  VERSION=$(git describe --exact-match HEAD)
+  EXIT_CODE=$?
+
+  if [ "$EXIT_CODE" == "128" ]; then
+    echo "Git commit does not have tag, cannot use version to build from"
+    exit 1
+  fi
+  echo "$VERSION"
+)
 
 echo "Archive contents:"
 cd $DESTINATION
 du -ah $DESTINATION
 cd - > /dev/null
 
-GZIP_FILE="dugite-native-$VERSION-win32-test.tar.gz"
-LZMA_FILE="dugite-native-$VERSION-win32-test.lzma"
+BUILD_HASH=$(git rev-parse --short HEAD)
 
+if ! [ -d "$DESTINATION" ]; then
+  echo "No output found, exiting..."
+  exit 1
+fi
+
+if [ "$TARGET_PLATFORM" == "ubuntu" ]; then
+  GZIP_FILE="dugite-native-$VERSION-$BUILD_HASH-ubuntu.tar.gz"
+  LZMA_FILE="dugite-native-$VERSION-$BUILD_HASH-ubuntu.lzma"
+elif [ "$TARGET_PLATFORM" == "macOS" ]; then
+  GZIP_FILE="dugite-native-$VERSION-$BUILD_HASH-macOS.tar.gz"
+  LZMA_FILE="dugite-native-$VERSION-$BUILD_HASH-macOS.lzma"
+elif [ "$TARGET_PLATFORM" == "win32" ]; then
+  if [ "$WIN_ARCH" -eq "64" ]; then ARCH="x64"; else ARCH="x86"; fi
+  GZIP_FILE="dugite-native-$VERSION-$BUILD_HASH-windows-$ARCH.tar.gz"
+  LZMA_FILE="dugite-native-$VERSION-$BUILD_HASH-windows-$ARCH.lzma"
+elif [ "$TARGET_PLATFORM" == "arm64" ]; then
+  GZIP_FILE="dugite-native-$VERSION-$BUILD_HASH-arm64.tar.gz"
+  LZMA_FILE="dugite-native-$VERSION-$BUILD_HASH-arm64.lzma"
+else
+  echo "Unable to package Git for platform $TARGET_PLATFORM"
+  exit 1
+fi
+
+(
 echo ""
-echo "Creating archives..."
-if [ "$(uname -s)" == "Darwin" ]; then
-  tar -czf $GZIP_FILE -C $DESTINATION .
-  tar --lzma -cf $LZMA_FILE -C $DESTINATION .
+PLATFORM=$(uname -s)
+echo "Creating archives for $PLATFORM (${OSTYPE})..."
+mkdir output
+cd output || exit 1
+if [ "$PLATFORM" == "Darwin" ]; then
+  echo "Using bsdtar which has some different command flags"
+  tar -czf "$GZIP_FILE" -C $DESTINATION .
+  tar --lzma -cf "$LZMA_FILE" -C $DESTINATION .
+elif [[ "$OSTYPE" == "msys" ]] || [[ "$OSTYPE" == "cygwin" ]]; then
+  tar -caf "$GZIP_FILE" -C $DESTINATION .
+  # hacking around the fact that 7z refuses to write to LZMA files despite them
+  # being the native format of 7z files
+  NEW_LZMA_FILE="dugite-native-$VERSION-win32-test.7z"
+  7z u -t7z "$NEW_LZMA_FILE" $DESTINATION/*
+  mv $NEW_LZMA_FILE $LZMA_FILE
 else
-  tar -caf $GZIP_FILE -C $DESTINATION .
-  tar -caf $LZMA_FILE -C $DESTINATION .
+  echo "Using unix tar by default"
+  tar -caf "$GZIP_FILE" -C $DESTINATION .
+  tar -caf "$LZMA_FILE" -C $DESTINATION .
 fi
 
-if [ "$APPVEYOR" == "True" ]; then
-  GZIP_CHECKSUM=$(sha256sum $GZIP_FILE | awk '{print $1;}')
-  LZMA_CHECKSUM=$(sha256sum $LZMA_FILE | awk '{print $1;}')
-else
-  GZIP_CHECKSUM=$(shasum -a 256 $GZIP_FILE | awk '{print $1;}')
-  LZMA_CHECKSUM=$(shasum -a 256 $LZMA_FILE | awk '{print $1;}')
-fi
+GZIP_CHECKSUM=$(compute_checksum "$GZIP_FILE")
+LZMA_CHECKSUM=$(compute_checksum "$LZMA_FILE")
 
-GZIP_SIZE=$(du -h $GZIP_FILE | cut -f1)
-LZMA_SIZE=$(du -h $LZMA_FILE | cut -f1)
+GZIP_SIZE=$(du -h "$GZIP_FILE" | cut -f1)
+LZMA_SIZE=$(du -h "$LZMA_FILE" | cut -f1)
 
 echo "${GZIP_CHECKSUM}" | tr -d '\n' > "${GZIP_FILE}.sha256"
 echo "${LZMA_CHECKSUM}" | tr -d '\n' > "${LZMA_FILE}.sha256"
@@ -48,3 +93,4 @@ echo "${LZMA_CHECKSUM}" | tr -d '\n' > "${LZMA_FILE}.sha256"
 echo "Packages created:"
 echo "${GZIP_FILE} - ${GZIP_SIZE} - checksum: ${GZIP_CHECKSUM}"
 echo "${LZMA_FILE} - ${LZMA_SIZE} - checksum: ${LZMA_CHECKSUM}"
+)
