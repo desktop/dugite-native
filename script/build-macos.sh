d261adb52f3f25eeb0acc76e24def487768f51aa
evelyn masso <outofambit@github.com>
2020-02-07 09:28:32 -0800

    build git-lfs ourselves

--- script/build-macos.sh
+++ script/build-macos.sh
@@ -13,16 +13,16 @@ if [[ -z "${DESTINATION}" ]]; then
   exit 1
 fi
 
-CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # shellcheck source=script/compute-checksum.sh
 source "$CURRENT_DIR/compute-checksum.sh"
 
 echo "-- Building git at $SOURCE to $DESTINATION"
 
 (
-cd "$SOURCE" || exit 1
-make clean
-DESTDIR="$DESTINATION" make strip install prefix=/ \
+  cd "$SOURCE" || exit 1
+  make clean
+  DESTDIR="$DESTINATION" make strip install prefix=/ \
     NO_PERL=1 \
     NO_TCLTK=1 \
     NO_GETTEXT=1 \
@@ -31,34 +31,23 @@ DESTDIR="$DESTINATION" make strip install prefix=/ \
     MACOSX_DEPLOYMENT_TARGET=10.10
 )
 
-
 if [[ "$GIT_LFS_VERSION" ]]; then
   echo "-- Bundling Git LFS"
-  GIT_LFS_FILE=git-lfs.tar.gz
-  GIT_LFS_URL="https://github.com/git-lfs/git-lfs/releases/download/v${GIT_LFS_VERSION}/git-lfs-darwin-amd64-v${GIT_LFS_VERSION}.tar.gz"
-  echo "-- Downloading from $GIT_LFS_URL"
-  curl -sL -o $GIT_LFS_FILE "$GIT_LFS_URL"
-  COMPUTED_SHA256=$(compute_checksum $GIT_LFS_FILE)
-  if [ "$COMPUTED_SHA256" = "$GIT_LFS_CHECKSUM" ]; then
-    echo "Git LFS: checksums match"
-    SUBFOLDER="$DESTINATION/libexec/git-core"
-    # strip out any text files when extracting the Git LFS archive
-    tar -xvf $GIT_LFS_FILE -C "$SUBFOLDER" --exclude='*.sh' --exclude='*.md'
-
-    if [[ ! -f "$SUBFOLDER/git-lfs" ]]; then
-      echo "After extracting Git LFS the file was not found under libexec/git-core/"
-      echo "aborting..."
-      exit 1
-    fi
+  git clone -b v$GIT_LFS_VERSION http://github.com/git-lfs/git-lfs
+  (
+    cd git-lfs
+    make CGO_CFLAGS="-mmacosx-version-min=10.10" CGO_LDFLAGS="-mmacosx-version-min=10.10" BUILTIN_LD_FLAGS="-linkmode external"
+  )
+  if test -f "git-lfs/bin/git-lfs"; then
+    cp git-lfs/bin/git-lfs $DESTINATION/libexec/git-core/
   else
-    echo "Git LFS: expected checksum $GIT_LFS_CHECKSUM but got $COMPUTED_SHA256"
+    echo "The git-lfs binary is missing, the build must have failed"
     echo "aborting..."
     exit 1
   fi
 else
   echo "-- Skipped bundling Git LFS (set GIT_LFS_VERSION to include it in the bundle)"
 fi
-
 
 echo "-- Removing server-side programs"
 rm "$DESTINATION/bin/git-cvsserver"
