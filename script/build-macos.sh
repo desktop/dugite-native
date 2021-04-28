f8d6252e02a65f046d165484f45bdb3146620a40
Sergio Padrino <sergio.padrino@gmail.com>
2021-04-28 13:46:31 +0200

    Run go generate using the host CPU

--- script/build-macos.sh
+++ script/build-macos.sh
@@ -76,6 +76,8 @@ if [[ "$GIT_LFS_VERSION" ]]; then
   git clone -b "v$GIT_LFS_VERSION" "https://github.com/git-lfs/git-lfs"
   (
     cd git-lfs
+    GO_GENERATE_STRING='$(GO) generate github.com\/git-lfs\/git-lfs\/commands'
+    sed -i -e "s/$GO_GENERATE_STRING/GOARCH= $GO_GENERATE_STRING/" Makefile
     make GOARCH="$GOARCH" CGO_CFLAGS="-mmacosx-version-min=$MACOSX_BUILD_VERSION" CGO_LDFLAGS="-mmacosx-version-min=$MACOSX_BUILD_VERSION" BUILTIN_LD_FLAGS="-linkmode external"
   )
   GIT_LFS_BINARY_PATH="git-lfs/bin/git-lfs"
