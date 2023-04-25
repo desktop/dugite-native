cac565c11c129a3cb56cb203b2b1bbc6b303036e
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2023-04-25 15:18:39 -0400

    add header modifications to sysroot for lower glibc compatibility
    restores compatibility with distros with GLIBC as low as 2.17 for 64bit systems (x86_64 and arm64)
    
    32bit systems require glibc 2.28+ in order to have functional 2GB+ file support so will require GLIBC 2.28

--- script/build-ubuntu.sh
+++ script/build-ubuntu.sh
@@ -21,7 +21,12 @@ if [[ -z "${CURL_INSTALL_DIR}" ]]; then
 fi
 
 case "$TARGET_ARCH" in
-  "x64") 
+  "x64")
+    # __GLIBC_MINOR__ is used as a feature test macro.  Replace it with the
+    # earliest supported version of glibc 2.17 as was previously the case when building on ubuntu-18.04
+    sudo sed -i 's|\(#define\s\+__GLIBC_MINOR__\)|\1 17 //|' "/usr/include/features.h"
+    # fcntl64() was introduced in glibc 2.28.  Make sure to use fcntl() instead.
+    sudo  sed -i '{N; s/#ifndef __USE_FILE_OFFSET64\(\nextern int fcntl\)/#if 1\1/}' "/usr/include/fcntl.h"
     DEPENDENCY_ARCH="amd64"
     export CC="gcc"
     STRIP="strip"
@@ -34,12 +39,17 @@ case "$TARGET_ARCH" in
     HOST="--host=i686-linux-gnu"
     TARGET="--target=i686-linux-gnu" ;;
   "arm64")
+    # __GLIBC_MINOR__ is used as a feature test macro.  Replace it with the
+    # earliest supported version of glibc 2.17 as was previously the case when building on ubuntu-18.04
+    sudo sed -i 's|\(#define\s\+__GLIBC_MINOR__\)|\1 17 //|' "/usr/aarch64-linux-gnu/include/features.h"
+    # fcntl64() was introduced in glibc 2.28.  Make sure to use fcntl() instead.
+    sudo  sed -i '{N; s/#ifndef __USE_FILE_OFFSET64\(\nextern int fcntl\)/#if 1\1/}' "/usr/aarch64-linux-gnu/include/fcntl.h"
     DEPENDENCY_ARCH="arm64"
     export CC="aarch64-linux-gnu-gcc"
     STRIP="aarch64-linux-gnu-strip"
     HOST="--host=aarch64-linux-gnu"
     TARGET="--target=aarch64-linux-gnu" ;;
-  "arm")     
+  "arm")
     DEPENDENCY_ARCH="arm"
     export CC="arm-linux-gnueabihf-gcc"
     STRIP="arm-linux-gnueabihf-strip"
