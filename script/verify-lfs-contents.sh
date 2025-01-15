39efe16ad169699706acedad360dece0338c1b5e
Markus Olsson <j.markus.olsson@gmail.com>
2025-01-15 14:33:28 +0100

    Update verify-lfs-contents.sh

--- script/verify-lfs-contents.sh
+++ script/verify-lfs-contents.sh
@@ -24,6 +24,7 @@ verify_lfs_contents() {
   # their release packages. Note that this only looks that the top
   # (technically second) level folder so new stuff in the man folder won't
   # get caught here.
+  # shellcheck disable=SC2015
   grep -qvE "^(CHANGELOG\.md|README\.md|git-lfs(\.exe)?|install\.sh|man)$" <<< "$TOPLEVEL" && {
     echo "Git LFS: unexpected files in the LFS archive, aborting..."
     exit 1
