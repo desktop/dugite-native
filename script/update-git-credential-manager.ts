81f7330a1e9b1b962d8a1925a6e2e1a5c72d74e3
Markus Olsson <j.markus.olsson@gmail.com>
2026-02-18 10:45:41 +0100

    Catch errors and exit appropriately

--- script/update-git-credential-manager.ts
+++ script/update-git-credential-manager.ts
@@ -6,6 +6,12 @@ import { coerceVersionPrefix } from './lib/coerce-version'
 
 process.on('unhandledRejection', reason => {
   console.error(reason)
+  process.exit(1)
+})
+
+process.on('uncaughtException', error => {
+  console.error(error)
+  process.exit(1)
 })
 
 async function run(): Promise<boolean> {
@@ -94,4 +100,9 @@ async function run(): Promise<boolean> {
   return true
 }
 
-run().then(success => process.exit(success ? 0 : 1))
+run()
+  .catch(error => {
+    console.error(error)
+    return false
+  })
+  .then(success => process.exit(success ? 0 : 1))
